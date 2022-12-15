71360c1112ed6dbcb94d5947066cbfd7dbefa496
etlas <etlessayahe@gmail.com>
2022-12-15 09:14:49 +0300

    first commit

---
+++ arguments.py
@@ -0,0 +1,27 @@
+import sys
+
+def hello(*args):
+    print(type(args))
+    print("hello", args[2], args[1], args[0])  # *args is atuple
+
+
+hello("ziay", 32, 1990)
+
+
+def hi(**kwargs):
+    print(type(kwargs))  # **kwargs is a dictonary
+    # print(
+    # f"hello {kwargs['first_name']}, you are {kwargs['age']}, you are a {kwargs['major']}")
+
+    # for valu in kwargs.values():
+    #     print(valu)
+
+    # for keys in kwargs.keys():
+    #     print(keys)
+#     for key, value in kwargs.items():
+#         print(key + ": " + str(value))
+
+
+# hi(first_name="ziya", age=32, major="programmer")
+
+print(sys.executable)
