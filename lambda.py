56a246cbc51391a14e6d4d460492765790c00570
ziya <koltikin210@gmail.com>
2023-07-17 11:31:14 +0300

    lambda useage

--- lambda.py
+++ lambda.py
@@ -1,10 +1,23 @@
 import functools
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# rsult = reduce(lambda x, y,: x * y, my_list)
-# print(rsult)
+my_list = [1, 2, 3, 4, 5, 6]
 
-print(functools.reduce(lambda x, y: x * y, my_list))
+rsult = functools.reduce(lambda x, y,: x * y, my_list)
+print(rsult)
+
+# print(functools.reduce(lambda x, y: x * y, my_list))
 
 
-name = input("what is your name?: ")
+# name = input("what is your name?: ")
+
+# print(functools.reduce(lambda x, y: x + " & " + y, name))
+
+
+def calculate(x, y, fun):
+    print(fun(x, y))
+
+
+calculate(8, 9, lambda x, y: x + y)
+calculate(18, 6, lambda x, y: x - y)
+calculate(13, 5, lambda x, y: x * y)
+calculate(26, 9, lambda x, y: x / y)
